1bb984d07431ea0315b1e62d7c0232697b80e37c
ThomasBakaysaJr <thomasbakaysajr@gmail.com>
2026-02-04 12:53:21 -0600

    Fix: Update projectDir, used hypen instead of underscore

--- android_app/settings.gradle.kts
+++ android_app/settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Corkboard-Capstone-App"
 include(":app")
-project(":app").projectDir = File("android-app")
+project(":app").projectDir = File("android_app")
