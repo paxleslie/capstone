9b983d4e971cd1d8b05a4fae5072ea2235f67105
scotty roberts <scottyroberts26@gmail.com>
2026-02-23 13:51:41 -0600

    Created pages corresponding to the buttons

--- android_app/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ android_app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,7 +22,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
+// import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -60,10 +61,12 @@ fun MyApplicationApp() {
         }
     ) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+            when (currentDestination) {
+                AppDestinations.HOME -> HomePage(modifier = Modifier.padding(innerPadding))
+                AppDestinations.FAVORITES -> FavoritesPage(modifier = Modifier.padding(innerPadding))
+                AppDestinations.PROFILE -> ProfilePage(modifier = Modifier.padding(innerPadding))
+                AppDestinations.STORE -> StorePage(modifier = Modifier.padding(innerPadding))
+            }
         }
     }
 }
@@ -75,20 +78,37 @@ enum class AppDestinations(
     HOME("Home", Icons.Default.Home),
     FAVORITES("Favorites", Icons.Default.Favorite),
     PROFILE("Profile", Icons.Default.AccountBox),
+    STORE(label = "Store", Icons.Default.ShoppingCart)
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun HomePage(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Home Page",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+fun FavoritesPage(modifier: Modifier = Modifier) {
+    Text(
+        text = "Favorites Page",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ProfilePage(modifier: Modifier = Modifier) {
+    Text(
+        text = "Profile Page",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun StorePage(modifier: Modifier = Modifier) {
+    Text(
+        text = "Store Page",
+        modifier = modifier
+    )
 }
